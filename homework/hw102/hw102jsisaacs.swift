ade6559fee10ae0a89b4fd24b7cbc72e0781d175
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-02 01:12:14 -0400

    tested useful Swift code

--- homework/hw102/hw102jsisaacs.swift
+++ homework/hw102/hw102jsisaacs.swift
@@ -1,9 +1,15 @@
-//
-//  hw102jsisaacs.swift
-//  
-//
-//  Created by Josh Isaacson - Work on 9/2/17.
-//
-//
-
 import Foundation
+
+func randomInRange (_ pMin: Int, _ pMax: Int) -> Int {
+    return pMin + Int(arc4random_uniform(UInt32(pMax - pMin + 1)))
+}
+
+print ("type something now")
+
+var inputText = readLine()!
+
+print ("you typed \(inputText)")
+
+var aRandomNumber = randomInRange(1, 3)
+
+print ("here's number \(aRandomNumber)")
