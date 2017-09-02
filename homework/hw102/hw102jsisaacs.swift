51f693551c3b07eb8e8941d73499abbe7f907823
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-02 18:24:30 -0400

    made progress on homework02

--- homework/hw102/hw102jsisaacs.swift
+++ homework/hw102/hw102jsisaacs.swift
@@ -1,15 +1,62 @@
 import Foundation
 
+//
 func randomInRange (_ pMin: Int, _ pMax: Int) -> Int {
     return pMin + Int(arc4random_uniform(UInt32(pMax - pMin + 1)))
 }
 
+//
+var randNumber = randomInRange(1, 100)
+print("Guess a number between 1 and 100")
+var guessCount = 1
+
+var num: Int = 0
+//
+
+/*
+if var userInput = readLine() {
+    if var num = Int(userInput) {
+        print("you typed \(num)")
+    }
+}
+*/
+
+let userInput = readLine()!
+var num = Int(userInput)
+
+print(num)
+
+//
+func randomNumberGame() {
+    //
+    /*
+    while num != randNumber {
+        if num < randNumber {
+            print("Guess ⬆︎")
+            guessCount += 1
+        }
+        //if num > randNumber {
+        //    print("Guess ⬇︎")
+        //    guessCount += 1
+        //}
+        //print("It took you " + guessCount + "guesses!")
+    }
+    */
+}
+
+
+
+
+/*
 print ("type something now")
 
 var inputText = readLine()!
 
 print ("you typed \(inputText)")
 
-var aRandomNumber = randomInRange(1, 3)
+//var aRandomNumber = randomInRange(1, 3)
 
 print ("here's number \(aRandomNumber)")
+ 
+*/
+ 
