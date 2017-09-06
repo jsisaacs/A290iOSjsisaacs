533c0578ab218f2ee4dcd7ea7e370b442eadef16
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-06 19:02:09 -0400

    lab105 part1

---
+++ labs/lab105/EventsAndTouches/EventsAndTouches/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventsAndTouches
+//
+//  Created by Josh Isaacson - Work on 9/6/17.
+//  Copyright © 2017 A290. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
