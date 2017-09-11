9ee2d07bc4d1c7d88f9684c48d1978ece3748479
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-11 19:05:17 -0400

    copied EventsTouches, added to lab106

---
+++ labs/lab106/EventsAndTouches/EventsAndTouches/ConcentratedView.swift
@@ -0,0 +1,13 @@
+//
+//  ConcentratedView.swift
+//  EventsAndTouches
+//
+//  Created by Josh Isaacson - Work on 9/11/17.
+//  Copyright © 2017 A290. All rights reserved.
+//
+
+import UIKit
+
+class ConcentratedView: UIView {
+    
+}
