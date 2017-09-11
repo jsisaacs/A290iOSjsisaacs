76ecd73fcd4fcaa7182cef65f544be78ea435e13
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-11 18:55:06 -0400

    touch event app, lab 106

--- labs/lab105/EventsAndTouches/EventsAndTouches/ViewController.swift
+++ labs/lab105/EventsAndTouches/EventsAndTouches/ViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var tapsLabel: UILabel!
+    @IBOutlet var touchesLabel: UILabel!
+    
+    func updateLabelsFromTouches(_ touches: NSSet) {
+        let theTouchObject = touches.anyObject() as! UITouch
+        let theNumOfTaps = theTouchObject.tapCount
+        let theTapsMessage = "\(theNumOfTaps) taps detected in sequence"
+        self.tapsLabel.text = theTapsMessage
+        let theNumOfTouches = touches.count
+        let theTouchesMessage = "\(theNumOfTouches) touches detected at once"
+        self.touchesLabel.text = theTouchesMessage
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.messageLabel.text = "Touches Began"
+        updateLabelsFromTouches(event!.allTouches! as NSSet)
+        if let firstTouch = touches.first {
+            let gestureStartPoint = firstTouch.location(in: self.view)
+            self.messageLabel.text =
+                "Touches Began at \(gestureStartPoint.x), \(gestureStartPoint.y)"
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.messageLabel.text = "Touches Moved"
+        updateLabelsFromTouches(event!.allTouches! as NSSet)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.messageLabel.text = "Touches Ended"
+        updateLabelsFromTouches(event!.allTouches! as NSSet)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.messageLabel.text = "Touches Cancelled"
+        updateLabelsFromTouches(event!.allTouches! as NSSet)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ labs/lab106/readme-lab106-jsisaacs.txt
@@ -0,0 +1,9 @@
+I observed my app to record how many clicks in sequence were made, touches began, and oddly only 1 touches detected at once.
+This is unexpected because I thought it would record clicks with multiple touches.
+
+The four event handlers can be triggered by clicking (touchesBegan), 
+moving while clicking (touchesMoved), unclicking (touchesEnded), and stopping
+the sequence of clicks (touchesCancelled).
+
+I guess forget the first paragraph it turns out if i hold option I can use 2 fingers.
+
