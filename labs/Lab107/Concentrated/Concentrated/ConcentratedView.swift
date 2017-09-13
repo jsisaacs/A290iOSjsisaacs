11fb0b55cc66027d55ab8fc703508dbb97aa0d4c
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-13 18:59:50 -0400

    lab 107

---
+++ labs/Lab107/Concentrated/Concentrated/ConcentratedView.swift
@@ -0,0 +1,21 @@
+//
+//  ConcentratedView.swift
+//  Concentrated
+//
+//  Created by Josh Isaacson - Work on 9/13/17.
+//  Copyright © 2017 A290 Fall 2017 - jsisaacs. All rights reserved.
+//
+
+import UIKit
+
+class ConcentratedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
